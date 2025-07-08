6707be088634f662c29b640d1c39f9f51aea56ee
Viliam Vadocz <viliam.vadocz@gmail.com>
2025-07-09 01:30:04 +0200

    Add advance() method for color

--- src/color.zig
+++ src/color.zig
@@ -2,10 +2,14 @@ pub const Color = enum(u1) {
     White = 0,
     Black = 1,
 
-    fn next(self: Color) Color {
+    pub fn next(self: Color) Color {
         return switch (self) {
             .White => .Black,
             .Black => .White,
         };
+    }
+
+    pub fn advance(self: *Color) void {
+        self.* = self.next();
     }
 };
