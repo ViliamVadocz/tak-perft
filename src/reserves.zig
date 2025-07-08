e88803ec09757ed2395d537fb8d8187218dff3f9
Viliam Vadocz <viliam.vadocz@gmail.com>
2025-07-09 01:29:46 +0200

    Swap capstone and flats in reserves

--- src/reserves.zig
+++ src/reserves.zig
@@ -3,12 +3,12 @@ const state = @import("state.zig");
 pub fn Reserves(n: comptime_int) type {
     state.assertSize(n);
     return switch (n) {
-        3 => packed struct { caps: u0 = 0, flats: u4 = 10 },
-        4 => packed struct { caps: u0 = 0, flats: u4 = 15 },
-        5 => packed struct { caps: u1 = 1, flats: u5 = 21 },
-        6 => packed struct { caps: u1 = 1, flats: u5 = 30 },
-        7 => packed struct { caps: u2 = 2, flats: u6 = 40 },
-        8 => packed struct { caps: u2 = 2, flats: u6 = 50 },
+        3 => packed struct { flats: u4 = 10, caps: u0 = 0 },
+        4 => packed struct { flats: u4 = 15, caps: u0 = 0 },
+        5 => packed struct { flats: u5 = 21, caps: u1 = 1 },
+        6 => packed struct { flats: u5 = 30, caps: u1 = 1 },
+        7 => packed struct { flats: u6 = 40, caps: u2 = 2 },
+        8 => packed struct { flats: u6 = 50, caps: u2 = 2 },
         else => unreachable,
     };
 }
