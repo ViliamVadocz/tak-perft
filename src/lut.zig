d70fa33c91fa4f9a86fb5fb53d387a5de37772ee
Viliam Vadocz <viliam.vadocz@gmail.com>
2025-07-10 12:28:50 +0200

    Add derivation for LUT

--- src/lut.zig
+++ src/lut.zig
@@ -1,3 +1,54 @@
+//! Render the following in Typst:
+//!
+//! = Counting Spreads
+//! == Variables
+//! - board size $n$
+//! - stack size $s$
+//! - amount we can pick up $k := min(n, s)$
+//! - distance available for spread $d$
+//!
+//! == Idea
+//! We start by picking up $k$ pieces, and then we make a binary choice for each:
+//! - drop the piece on the current square
+//! - move one stop and drop the piece there
+//! We can represent a spread as $k$-bit number (a pattern), where each bit is set
+//! when we choose to move (choices start at lowest significant bit). A valid spread
+//! has to move at least one stone, so a valid pattern is $>= 1$. We can also choose
+//! to move with every since piece,which would give $k$ set bits = $2^k - 1$.
+//!
+//! When we restrict the distance available, we are essentially restricting how many
+//! times we can choose to take a step, so in the binary representation, how many set
+//! bits we have in the pattern.
+//!
+//! == Formula
+//! $
+//! "spreads"(k, d)
+//!   &= sum_(p=1)^(2^k - 1) ["popcnt"(p) <= d] \
+//!   &= cases(
+//!     2^k - 1 &"if" k <= d,
+//!     sum_(p=1)^(2^ k) ["popcnt"(p) <= d] &"else"
+//!   )\
+//!   &= cases(
+//!     2^k - 1 &"if" k <= d,
+//!     sum_(i=1)^d binom(k, i) &"else"
+//!   )
+//! $
+//!
+//! Since both $k$ and $d$ is bounded by $n$, we can make a LUT for the second case.
+//!
+//! == Smashing
+//!
+//! If the stack has a capstone on top, we are able to smash walls, which adds
+//! additional ways we can spread. These spreads correspond to all those that reach
+//! a distance $d + 1$ and end with a move.
+//!
+//! $
+//! "smashes"(k, d)
+//!   &= sum_(p=1)^(2^k - 1) ["popcnt"(p) = d + 1 and k"th bit is set in" p]\
+//!   &= sum_(p=2^(k-1))^(2^k - 1) ["popcnt"(p) = d + 1]\
+//!   &= binom(k - 1, d)\
+//! $
+
 const std = @import("std");
 const state = @import("state.zig");
 
